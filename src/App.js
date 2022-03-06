26937e853efa7316167887705b6c04579d1709fb
carpben <bccarp@gmail.com>
2022-03-06 13:34:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Box from "./Box.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   state = {
+      numberOfBoxes: 0,
+   };
+   inputRef = React.createRef();
+
+   onCreate = () => {
+      this.setState({
+         numberOfBoxes: this.inputRef.current.valueAsNumber,
+      });
+   };
+
+   componentDidMount() {
+      console.log("component created");
+   }
+
+   componentDidUpdate() {
+      console.log("component updated");
+   }
+
+   render() {
+      return (
+         <div>
+            <div>
+               <input type="number" ref={this.inputRef} />
+               <button onClick={this.onCreate}>create</button>
+            </div>
+
+            {[...Array(this.state.numberOfBoxes)].map((_, i) => (
+               <Box key={i} />
+            ))}
+         </div>
+      );
+   }
 }
 
 export default App;
---
+++ src/Box.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./Box.css";
+
+function Box() {
+   const [lightened, setLightened] = useState(false);
+
+   return (
+      <div
+         className={`box ${lightened ? "light" : ""}`}
+         onClick={setInterval(() => {
+            console.log("running");
+            setLightened((currentLight) => !currentLight);
+         }, 2000)}
+      />
+   );
+}
+
+export default Box;
